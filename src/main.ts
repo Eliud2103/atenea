d788c925e1c7bcbfead9d59128304333bce84d78
Soy Eliud <diegoeliudgarciahernandez@gmail.com>
2025-03-04 22:26:14 -0600

    BACKEND: login ya manda a home ahora si

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,12 @@
-<<<<<<< HEAD
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: '*', // Esto permite que cualquier dominio haga solicitudes al servidor.
   });
   await app.listen(process.env.PORT ?? 3000);
-
 }
 bootstrap();
-=======
-import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-import { provideHttpClient } from '@angular/common/http';
-
-import { routes } from './app/app.routes';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(),
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy, },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
-  ],
-});
->>>>>>> 5addf3d28df915f8dcd1740eaebe74c4b7f31290
